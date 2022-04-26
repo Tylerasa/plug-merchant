a91d3a27e509096c8b1f530540137341790c8c84
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-26 20:11:42 +0000

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Databse connection established");
+});
